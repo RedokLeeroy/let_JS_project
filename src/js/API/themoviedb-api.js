4ea51996a7c092739f833b389e03718edf003662
andrii_h <galgayand@gmail.com>
2022-07-05 12:30:37 +0300

    done fetch

---
+++ src/js/API/themoviedb-api.js
@@ -0,0 +1,78 @@
+export class ThemoviedbAPI {
+  // static #BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+  // static #API_KEY = '?api_key=7a4cd4317772102a9b88ef6a54b71590';
+
+  #BASE_URL = 'https://api.themoviedb.org/3/';
+  #API_KEY = '?api_key=7a4cd4317772102a9b88ef6a54b71590';
+
+  #page = 1;
+  #keyword;
+
+  constructor(keyword = '') {
+    this.#keyword = keyword;
+  }
+
+  async getMovies() {
+    const trending = 'trending';
+    const mediaType = 'movie';
+    const timeWindow = 'day';
+
+    try {
+      return await fetch(
+        `${this.#BASE_URL}${trending}/${mediaType}/${timeWindow}${
+          this.#API_KEY
+        }`
+      ).then(res => res.json());
+    } catch (arr) {
+      console.log(err);
+    }
+  }
+
+  async getSertchMovie() {
+    this.#keyword = 'cat'; //тимчасово
+
+    const option = 'keyword';
+
+    const search = 'search';
+
+    try {
+      return await fetch(
+        `${this.#BASE_URL}${search}/${option}${this.#API_KEY}&query=${
+          this.#keyword
+        }&page=${this.#page}`
+      ).then(res => res.json());
+    } catch (arr) {
+      console.log(err);
+    }
+  }
+
+  resetPage() {
+    this.#page = 0;
+  }
+
+  get page() {
+    return this.#page;
+  }
+
+  set page(value) {
+    this.#page = value;
+  }
+
+  get keyword() {
+    return this.#keyword;
+  }
+
+  set keyword(value) {
+    this.#keyword = value;
+  }
+}
+
+//-------------------------------------------\\
+
+// const pixabayAPI = new PixabayAPI();
+
+// const keyword = searchQuery.value.trim();
+// //  pixabayAPI.page = 1;
+// pixabayAPI.keyword = keyword;
+
+// pixabayAPI.getIMGs().then(handleSuccess).catch(handleError);
