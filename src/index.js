3652a7a8eb4148e36ff87849aab902c73947c902
andrii_h <galgayand@gmail.com>
2022-07-05 19:19:19 +0300

    11

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './sass/index.scss';
 import './js/componentsJs/modalFooter';
 
-// import { ThemoviedbAPI } from './js/API/themoviedb-api';
 import { getApiList } from './js/componentsJs/generateCardList';
-
-// const themoviedbAPI = new ThemoviedbAPI();
-
 
 getApiList('trending')
