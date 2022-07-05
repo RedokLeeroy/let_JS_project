d5da340c15a1e944fbcb49d1b833977ffc828682
RedokLeeroy <Lys1999@ukr.net>
2022-07-06 02:58:05 +0300

    doneP

--- src/js/componentsJs/generateCardList.js
+++ src/js/componentsJs/generateCardList.js
@@ -1,17 +1,37 @@
 // import { ThemoviedbAPI } from '.js/API/themoviedb-api';
 import { ThemoviedbAPI } from '../API/themoviedb-api';
 import { createCard } from './createCard';
-
-const gallery = document.querySelector('.gallery')
-
+import { renderPagination } from '../pagination/pagination';
+const gallery = document.querySelector('.gallery');
+const container = document.querySelector('.pagination-container');
 const themoviedbAPI = new ThemoviedbAPI();
+console.log(container);
 
-export function getApiList(apiRoute){
-  if(apiRoute === 'trending'){
-    themoviedbAPI.getMovies().then(handleSuccess)
+let currentPage = 1;
+
+export function getApiList(apiRoute) {
+  if (apiRoute === 'trending') {
+    themoviedbAPI.getMovies().then(handleSuccess);
   }
 }
 
-function handleSuccess(data){
-  gallery.insertAdjacentHTML('beforeend', createCard(data.results))
+function handleSuccess(data) {
+  gallery.innerHTML = createCard(data.results);
+  renderPagination(data.page, data.total_pages, themoviedbAPI.getMovies);
+  container.addEventListener('click', handlePagination);
+}
+
+function handlePagination(event) {
+  if (event.target.tagName !== 'LI' || event.target.textContent === '...') {
+    return;
+  }
+  const page = event.target.textContent;
+  if (page === '⇽') {
+    currentPage = Number(currentPage) - 1;
+  } else if (page === '⇾') {
+    currentPage = Number(currentPage) + 1;
+  } else {
+    currentPage = page;
+  }
+  themoviedbAPI.getMovies(currentPage).then(handleSuccess);
 }
--- src/js/pagination/refs.js
+++
@@ -1,7 +0,0 @@
-export default refs;
-refs = {
-  container: document.querySelector('.pagination-container'),
-  leftArrow: document.querySelector('.left-arrow'),
-  list: document.querySelector('.buttonList'),
-  rightArrow: document.querySelector('.rightArrow'),
-};
