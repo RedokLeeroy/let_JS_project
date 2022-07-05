b4249eaeae05640f0ab88392a7b4adde2d53f63c
RoRomario360 <fotoromario@gmail.com>
2022-07-05 19:39:53 +0300

    ccommit-andre

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './sass/index.scss';
 import { renderModalFilm } from './js/templates/renderModalFilm';
+import callModal from './js/componentsJs/modalFooter';
 renderModalFilm();
-import './js/componentsJs/modalFooter';
 
-import { ThemoviedbAPI } from './js/API/themoviedb-api';
+// import { ThemoviedbAPI } from './js/API/themoviedb-api';
 
-const themoviedbAPI = new ThemoviedbAPI();
+// const themoviedbAPI = new ThemoviedbAPI();
