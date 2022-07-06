4e412dd94eca7a1abfb990956d172fd0048e14c1
RedokLeeroy <Lys1999@ukr.net>
2022-07-06 03:46:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import { renderModalFilm } from './js/templates/renderModalFilm';
 import { getApiList, getData } from './js/componentsJs/generateCardList';
 import callModal from './js/componentsJs/modalFooter';
 
-import {newData} from './js/componentsJs/generateCardList'
+import { newData } from './js/componentsJs/generateCardList';
 
 renderModalFilm();
 
-console.log(getData());
-getApiList('trending')
+getApiList('trending');
