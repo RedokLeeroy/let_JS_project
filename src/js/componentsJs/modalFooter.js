e47464ddabfafe62765c8cbdf1a777ef3166b814
AndriiSkrypka <andriskrupka25011994@icloud.com>
2022-07-06 18:39:52 +0300

    foter-modal

--- src/js/componentsJs/modalFooter.js
+++ src/js/componentsJs/modalFooter.js
@@ -10,6 +10,11 @@ function toggleModal(event) {
   event.preventDefault();
   refs.modal.classList.toggle('is-hidden');
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  if (document.body.style.overflow === 'hidden') {
+    document.body.style.overflow = 'auto';
+  } else {
+    document.body.style.overflow = 'hidden';
+  }
 }
 export default function callModal() {
   document.addEventListener('keydown', event => {
