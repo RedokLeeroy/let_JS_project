0414ed88c95cec9e4e001ceb9e7d044443771d69
andrii_h <galgayand@gmail.com>
2022-07-06 11:55:15 +0300

    Update generateCardList.js

--- src/js/componentsJs/generateCardList.js
+++ src/js/componentsJs/generateCardList.js
@@ -1,11 +1,10 @@
-// import { ThemoviedbAPI } from '.js/API/themoviedb-api';
 import { ThemoviedbAPI } from '../API/themoviedb-api';
 import { createCard } from './createCard';
 import { renderPagination } from '../pagination/pagination';
+
 const gallery = document.querySelector('.gallery');
 const container = document.querySelector('.pagination-container');
 const themoviedbAPI = new ThemoviedbAPI();
-console.log(container);
 
 let currentPage = 1;
 
