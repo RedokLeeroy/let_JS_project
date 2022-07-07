098bb6323f9368be617fbc163a559bbc473ae691
Lika <lika.rossinskaya@gmal.com>
2022-07-07 14:25:41 +0300

    fixed bugs in header and  myLibrary btn

--- src/js/componentsJs/library.js
+++ src/js/componentsJs/library.js
@@ -7,6 +7,10 @@ const watchedCards = JSON.parse(localStorage.getItem(LIBRARY_WATCHED));
 const queuedCards = JSON.parse(localStorage.getItem(LIBRARY_QUEUE));
 const watchedBtn = document.getElementById('watched-button');
 const queueBtn = document.getElementById('queue-button');
+const main = document.querySelector('main');
+main.style.height = '100vh';
+
+gallery.innerHTML = createCard(watchedCards);
 
 watchedBtn.addEventListener('click', watchCards);
 queueBtn.addEventListener('click', queueCards);
