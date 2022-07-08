90b946485e693cb1c6e869d4f8b5ad72b00f2707
Lika <lika.rossinskaya@gmal.com>
2022-07-08 11:38:24 +0300

    removed code of Andrey

--- src/js/componentsJs/library.js
+++ src/js/componentsJs/library.js
@@ -1,6 +1,4 @@
 import { createCard } from './createCard';
-import { handleSuccess } from './generateCardList';
-import { renderModalFilm } from '../templates/renderModalFilm';
 
 const LIBRARY_WATCHED = 'library-watched';
 const LIBRARY_QUEUE = 'library-queie';
@@ -9,39 +7,13 @@ const watchedCards = JSON.parse(localStorage.getItem(LIBRARY_WATCHED));
 const queuedCards = JSON.parse(localStorage.getItem(LIBRARY_QUEUE));
 const watchedBtn = document.getElementById('watched-button');
 const queueBtn = document.getElementById('queue-button');
-const main = document.querySelector('main');
 const libraryPlaceholder = document.querySelector('.placeholder');
 
-let currentPage = 1;
-
-const localObj = {
-  page: currentPage,
-  results: [],
-  total_pages: 0,
-  inLocalStorage: true,
-};
+watchedBtn.classList.add('active');
+gallery.innerHTML = createCard(watchedCards);
 
 if (watchedCards.length !== 0) {
-  watchedCards.forEach(element => {
-    localObj.results.push(element);
-  });
-
-  const arr = handleSuccess(localObj);
-  renderModalFilm(arr);
-}
-if (queuedCards.length !== 0) {
-  queuedCards.forEach(element => {
-    localObj.results.push(element);
-  });
-
-  const arr = handleSuccess(localObj);
-  renderModalFilm(arr);
-}
-
-gallery.innerHTML = createCard(watchedCards);
-if (watchedCards.length) {
   libraryPlaceholder.style.display = 'none';
-  gallery.innerHTML = createCard(watchedCards);
 }
 
 watchedBtn.addEventListener('click', watchCards);
@@ -51,16 +23,19 @@ function watchCards(evt) {
   evt.preventDefault();
   togglePlaceholder(watchedCards);
   gallery.innerHTML = createCard(watchedCards);
+  queueBtn.classList.remove('active');
+  watchedBtn.classList.add('active');
 }
 
 function queueCards(evt) {
   evt.preventDefault();
   togglePlaceholder(queuedCards);
   gallery.innerHTML = createCard(queuedCards);
+  watchedBtn.classList.remove('active');
+  queueBtn.classList.add('active');
 }
 
 function togglePlaceholder(collection) {
-  в;
   if (collection.length) {
     libraryPlaceholder.style.display = 'none';
   } else {
