8ad98c4ec8557afa4628317bf2e7ef551bf60607
Lika <lika.rossinskaya@gmal.com>
2022-07-08 21:08:48 +0300

    restoration

--- src/js/componentsJs/library.js
+++ src/js/componentsJs/library.js
@@ -8,8 +8,7 @@ const watchedCards = JSON.parse(localStorage.getItem(LIBRARY_WATCHED));
 const queuedCards = JSON.parse(localStorage.getItem(LIBRARY_QUEUE));
 const watchedBtn = document.getElementById('watched-button');
 const queueBtn = document.getElementById('queue-button');
-const main = document.querySelector('main');
-main.style.height = '100vh';
+const libraryPlaceholder = document.querySelector('.placeholder');
 
 let currentPage = 1;
 
@@ -21,31 +20,49 @@ const localObj = {
   isWatched: false,
 };
 
-// watchCards();
+watchCards();
+if (watchedCards.length !== 0) {
+  libraryPlaceholder.style.display = 'none';
+}
 
 watchedBtn.addEventListener('click', watchCards);
 queueBtn.addEventListener('click', queueCards);
 
 function watchCards(evt) {
+  togglePlaceholder(watchedCards);
   gallery.innerHTML = '';
   if (watchedCards.length !== 0) {
     watchedCards.forEach(element => {
       localObj.results.push(element);
     });
-
+    //   togglePlaceholder(watchedCards);
+    // gallery.innerHTML = '';
     const arr = handleSuccess(localObj);
     renderModalFilm(arr);
+    queueBtn.classList.remove('active');
+    watchedBtn.classList.add('active');
   }
 }
 
 function queueCards(evt) {
   gallery.innerHTML = '';
+  togglePlaceholder(queuedCards);
   if (queuedCards.length !== 0) {
     queuedCards.forEach(element => {
       localObj.results.push(element);
     });
-
+    //  togglePlaceholder(queuedCards);
     const arr = handleSuccess(localObj);
     renderModalFilm(arr);
+    watchedBtn.classList.remove('active');
+    queueBtn.classList.add('active');
+  }
+}
+
+function togglePlaceholder(collection) {
+  if (collection.length) {
+    libraryPlaceholder.style.display = 'none';
+  } else {
+    libraryPlaceholder.style.display = 'block';
   }
 }
