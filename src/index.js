a2b8b98a9864e169d9031034f4e19e721d302666
RoRomario360 <fotoromario@gmail.com>
2022-07-09 11:42:19 +0300

    scrool-pag

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { renderModalFilm } from './js/templates/renderModalFilm';
 import { getApiList } from './js/componentsJs/generateCardList';
 import callModal from './js/componentsJs/modalFooter';
 import { search } from './js/API/search';
-import arrowUpScroll from './js/componentsJs/scroolUp';
+import topFunction from './js/componentsJs/scroolUp';
+
 getApiList();
